483a9cfb2386e5b650224d4a1c808c4e4496451a
unknown <amityedage371@gmail.com>
2026-05-04 00:08:00 +0530

    Pascal Traingle advance version is done

--- pascal.cpp
+++ pascal.cpp
@@ -17,6 +17,22 @@ public:
         }
         
         return triangle;
+        class Solution {
+public:
+    vector<int> getRow(int rowIndex) {
+        // Initialize vector of size (rowIndex + 1) with all 1s
+        vector<int> row(rowIndex + 1, 1);
         
+        // Iterate through each row from 2 up to rowIndex
+        for (int i = 2; i <= rowIndex; ++i) {
+            // Update from right to left to use values from the previous state
+            for (int j = i - 1; j > 0; --j) {
+                row[j] = row[j] + row[j - 1];
+            }
+        }
+        
+        return row;
+    }
+};
     }
 };
