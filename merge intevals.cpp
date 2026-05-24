46093e43771903b45b180251a9039ccee7307704
unknown <amityedage371@gmail.com>
2026-05-25 02:02:32 +0530

    Some comment part is removed

--- merge intevals.cpp
+++ merge intevals.cpp
@@ -1,13 +1,10 @@
 class Solution {
 public:
-    //function to solve the merge interval 
-    vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        //Sort the intervals on the basis of start to begin 
+   
+    vector<vector<int>> merge(vector<vector<int>>& intervals) { 
         sort(intervals.begin(),intervals.end());
-
         //create the vector to store the final merged intervals
         vector<vector<int>> merged;
-
         //Traverse the intervals
         for(auto interval : intervals){
             //Check the merge interval is empty or current interval is not overlaping with other 
@@ -16,7 +13,7 @@ public:
                 merged.push_back(interval);
             }
             else{
-                //merge the overlaping interval
+               
                 merged.back()[1] = max ( merged.back()[1] , interval[1]);
 
             }
